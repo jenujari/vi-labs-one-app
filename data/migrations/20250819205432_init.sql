b2f1bf0228f1117fa63e8a2f641437cc00c87dc4
Jenish Jariwala <jhon.5456@gmail.com>
2025-08-24 23:40:08 +0100

    PostgreSQL setup (#1)

--- data/migrations/20250819205432_init.sql
+++ data/migrations/20250819205432_init.sql
@@ -1,12 +1,15 @@
 -- +goose Up
 -- +goose StatementBegin
-CREATE TABLE "tbl_key_value" (
-	"key"	TEXT,
-	"value"	TEXT
+CREATE UNLOGGED TABLE IF NOT EXISTS tbl_cache
+(
+    key character varying(100) NOT NULL,
+    value text,
+    created timestamp with time zone,
+    PRIMARY KEY (key)
 );
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
-DROP TABLE "tbl_key_value";
+DROP TABLE IF EXISTS "tbl_cache";
 -- +goose StatementEnd
